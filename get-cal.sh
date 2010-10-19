311c2d0973fbbeb35040c8356dcf64256c386fee
BKrsta <bkrsta@gmail.com>
2010-10-19 13:56:02 +0200

    fix

--- get-cal.sh
+++ get-cal.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-wget --quiet http://www.google.com/calendar/ical/81d23ab0r2mcll612eeqlgtd90@group.calendar.google.com/public/basic.ics && echo OK
+wget --quiet -O basic.ics \
+  http://www.google.com/calendar/ical/81d23ab0r2mcll612eeqlgtd90@group.calendar.google.com/public/basic.ics \
+  && echo OK
 
